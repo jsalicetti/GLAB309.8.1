cedf1872f6274619e42f4a2c7a448138dbecb63a
jsalicetti <jessicasalicetti@yahoo.com>
2024-09-18 12:01:52 -0400

    complete

---
+++ 309.8.1/src/test/java/com/lab/_981/SpringdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab._981;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
